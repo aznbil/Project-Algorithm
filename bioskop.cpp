2bd39282019d8a7d4c2d87533dc75adbf6351d62
aziz <aziznabil9859@gmail.com>
2025-03-11 14:34:06 +0700

    menambahkan struct, dan func

--- bioskop.cpp
+++ bioskop.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
 using namespace std;
 
+struct bioskop{
+  int id_film;
+  string judul;
+  string genre;
+};
+
+void quick_short();
+void menu();
+
 int main(){
   cout << "Hello World" << endl;
 }
+
+void menu(){
+  int pilih;
+
+  cout << "Menu" << endl;
+  cout << "1. Quick Sort" << endl;
+  cout << "2. Merge Sort" << endl;
+  cout << "3. Selection Sort" << endl;
+  cout << "4. Insertion Sort" << endl;
+  cout << "5. Bubble Sort" << endl;
+  cout << "6. Exit" << endl;
+  cout << "Masukkan Pilihan : ";
+  cin >> pilih;
+
+  switch(pilih){
+    case 1:
+      quick_short();
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    case 5:
+      break;
+    case 6:
+      break;
+    default:
+      cout << "Pilihan Salah" << endl;
+  }
+}
+
