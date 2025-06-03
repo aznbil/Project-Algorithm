52dc7107a1897323ecaa8c45d6bc30a4bcc8380f
aziz <aziznabil9859@gmail.com>
2025-06-03 22:51:06 +0700

    done

--- tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-cout << "\nJudul Film\t: " << film[i].judul << endl;
-        cout << "Genre Film\t: " << film[i].genre << endl;
-        cout << "Durasi (menit)\t: " << film[i].durasi << " menit" << endl;
-        cout << "Rating Film\t: " << film[i].rating << endl;
-        cout << "-----------------------" << endl;
-        cout << endl;
